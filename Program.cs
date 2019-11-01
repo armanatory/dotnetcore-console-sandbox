52d377c7e2233ea5fc380f948813de0b8dd53b13
Arman <armanatory@gmail.com>
2019-11-01 08:24:39 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dotnetcore_console_sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Please insert the first number:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Please insert the second number:");
+            string input2 = Console.ReadLine();
+
+            try{
+                num1 = Convert.ToInt16(input1);
+                num2 = Convert.ToInt16(input2);
+
+                Console.WriteLine("The summation is: " + num1+num2);
+            }
+            catch(FormatException){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fromat Error!");
+                Console.ResetColor();
+            }
+            catch{
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unknown Error!");
+                Console.ResetColor();
+            }
+            finally{
+                Console.WriteLine("The program has finished.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
